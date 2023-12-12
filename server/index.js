d7b06089c041c25534ba6dc4fbac6f04769e36fd
vswc <jakemackie31@gmail.com>
2023-12-12 23:51:08 +0000

    idek

---
+++ server/index.js
@@ -0,0 +1,17 @@
+
+const http = require('http').createServer();
+
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('message', (message) =>     {
+        console.log(message);
+        io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
+    });
+});
+
+http.listen(8080, () => console.log('listening on 127.0.0.1:8080') );
